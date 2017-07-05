7a4cbf31d62de3bdd2ae58b9f0714d83ba4deb90
Beacon Design <beacondesign@hotmail.com>
2017-07-05 18:39:01 +0800

    20170705

--- Python/Python Study Workspace/Python Study Projects/Python Files/Class/a more realistic example/person.py
+++ Python/Python Study Workspace/Python Study Projects/Python Files/Class/a more realistic example/person.py
@@ -1,25 +1,38 @@
-class Person:
+
+
+# File person.py (final)
+""" 
+Record and process information about people. Run this file directly to test its classes. 
+"""
+
+from classtools import AttrDisplay                  # Use generic display tool
+
+class Person(AttrDisplay):                          # Mix in a repr at this level
+    """ 
+    Create and process person records 
+    """
     def __init__(self, name, job=None, pay=0):      # Constructor takes three arguments
         self.name = name                            # Fill out fields when created
         self.job = job                              # self is the new instance object
         self.pay = pay
 
     # Add methods to encapsulate operations for maintainability
-    def lastName(self):                             # Behavior methods
+    def lastName(self):                             # Behavior methods, Assumes last is last
         return self.name.split()[-1]                # self is implied subject
-    def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent))    # Must change here only
 
-    # Add __repr__ overload method for printing objects
-    def __repr__(self):                              # Added method
-        return '[Person: %s, %s]' % (self.name, self.pay)       # String to print
+    def giveRaise(self, percent):                   # Percent must be 0..1
+        self.pay = int(self.pay * (1 + percent))    # Must change here only
 
 # Add customization of one behavior in a subclass
 class Manager(Person):
+    """
+    A customized Person with special requirements
+    """
 
     # Add customization of constructor in a subclass
     def __init__(self, name, pay):                   # Redefine constructor
-        Person.__init__(self, name, 'mgr', pay)     # Run original with 'mgr'
+        Person.__init__(self, name, 'mgr', pay)     # Run original with 'mgr'   Job name is implied
+
     def giveRaise(self, percent, bonus=.10):         # Redefine at this level
         Person.giveRaise(self, percent + bonus)     # Call Person's version
 
@@ -38,5 +51,3 @@ if __name__ == '__main__':                           # When run for testing only
     tom.giveRaise(.10)                       # Implied/set by class
     print(tom.lastName())
     print(tom)
-
-
